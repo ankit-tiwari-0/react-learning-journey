3a5125d3aea952cdc55c2861154093be8aed1db7
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2025-12-28 15:18:40 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,13 @@ function App(){
   return(
     <div>
       <h1>Event and function call</h1>
+      <button onClick={callFun}>nn</button>
       <button onClick={()=>fruit("apple")}>Appleeee</button>
       <button onClick={()=>fruit("apple")}>Banana</button>
     </div>
   )
-}
+} 
+
+// onclick call itself it want defination of function okk 
 
 export default App
