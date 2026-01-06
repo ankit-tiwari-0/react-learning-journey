26eafd376e7f0241745bfc1ca96eaf4e29fb836c
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-01-06 11:42:04 +0530

    Handle Checkbox in reactjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-import { useState } from "react"
+import Checkbox from "./skill";
 
 function App(){
-  const [name,setname]=useState('');
-  const [password,setpass]=useState('');
-  const [gmail,setgmail]=useState('');
     return(
         <div>
-            <h1>ConTroller component</h1>
-            <form action="" method="get">
-                <input type="text" onChange={(e)=>setname(e.target.value)} placeholder="enter name" />
-                <br />
-                <input type="text" onChange={(e)=>setpass(e.target.value)} placeholder="Password" />
-                <br />
-                <input type="text" onChange={(e)=>setgmail(e.target.value)} placeholder="Gmail" />
-                <br />
-                <button>submit</button>
-                <button onClick={()=>{setgmail('');setname();setpass()}}>clear</button>
-                <h4>{name}</h4>
-                <h4>{password}</h4>
-                <h4>{gmail}</h4>
-            </form>
+            <h1>Handle Checkbox in reactjs</h1>
+            <Checkbox />
         </div>
     )
 }
