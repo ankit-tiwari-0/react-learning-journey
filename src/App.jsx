215185150edf7b53739c917237c3dbdb13189e4c
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-01-08 20:01:53 +0530

    Dummy data without loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,97 @@
-import { useState } from "react";
-
-
-function App(){
-    const [gender, setgender]=useState("");
-    const [city, setcity]=useState("Noida");
-    return(
+function App() {
+    const users = [
+        {
+            name: 'ankit',
+            age: '14',
+            email: 'ani@gmail.com',
+            id: '1'
+        },
+        {
+            name: 'anki',
+            age: '14',
+            email: 'ani@gmail.com',
+            id: '2'
+        },
+        {
+            name: 'ant',
+            age: '10',
+            email: 'ani@gmail.com',
+            id: '4'
+        },
+        {
+            name: 'ank',
+            age: '14',
+            email: 'ani@gmail.com',
+            id: '4'
+        },
+        {
+            name: 'kit',
+            age: '15',
+            email: 'ani@gmail.com',
+            id: '5'
+        },
+    ]
+    return (
         <div>
-            <h1>Handle Radio and Dropdown</h1>
-            <hr />
-            <h2 style={{fontSize:"50px"}}>Select Genderwa</h2>
-            <input type="radio" onChange={(e)=>setgender(e.target.value)} name="gender" checked={gender== 'male'} id="male" value="male" style={{ transform: "scale(2.6)", margin: "10px", }} /> 
-            <label style={{fontSize: "20px", color: "red", margin: "10px"}} htmlFor="male">Male</label>
-
-            <input type="radio"  onChange={(e)=>setgender(e.target.value)} name="gender" id="female" value="female" style={{ transform: "scale(2.6)", margin: "20px", }} /> 
-            <label style={{fontSize: "20px", color: "pink", margin: "10px"}} htmlFor="female">Female</label>
-            <input type="radio"  onChange={(e)=>setgender(e.target.value)} name="gender" id="female" value="Non-binary" style={{ transform: "scale(2.6)", margin: "20px", }} /> 
-            <label style={{fontSize: "20px", color: "yellow", margin: "10px"}} htmlFor="Non-binary">Non-binary</label>
-            <input type="radio"  onChange={(e)=>setgender(e.target.value)} name="gender"value="other" id="other" style={{ transform: "scale(2.6)", margin: "20px", }} /> 
-            <label style={{fontSize: "20px", color: "blue", margin: "10px"}} htmlFor="other">Other</label>
-
-            <h2>selected Gender: {gender}</h2>
-            <h3 style={{marginTop:"50px", }}>Select city</h3>
-            <select onChange={(e)=>setcity(e.target.value)} defaultValue={"Noida"} style={{ transform: "scale(1.6)", margin: "20px", }} >
-                <option style={{color:"red",}} value="Noida">noida</option>
-                <option value="Delhi" >nnnnn</option>
-                <option value="Agra">dddd</option>
-                <option value="Usmanpur">aaaa</option>
-            </select>
-              <h2>Selected:{city}</h2>
-
+            <h1>Loop in jsx Map function </h1>
+            <h3>Dynamic data with loop</h3>
+            <table border={"2px"}>
+                <thead>
+                    <tr>
+                        <td>id</td>
+                        <td>name</td>
+                        <td>Gmail</td>
+                        <td>Age</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user) => (
+                            <tr key={user.id}>
+                                <td>{user.id}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.age}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+            <h3>Dummy data without loop</h3>
+            <table border={1}>
+                <thead>
+                    <tr>
+                        <td>id</td>
+                        <td>name</td>
+                        <td>email</td>
+                        <td>age</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>anil</td>
+                        <td>anil@gmail.com</td>
+                        <td>21</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>anil</td>
+                        <td>anil@gmail.com</td>
+                        <td>21</td>
+                    </tr><tr>
+                        <td>1</td>
+                        <td>anil</td>
+                        <td>anil@gmail.com</td>
+                        <td>21</td>
+                    </tr><tr>
+                        <td>1</td>
+                        <td>anil</td>
+                        <td>anil@gmail.com</td>
+                        <td>21</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
