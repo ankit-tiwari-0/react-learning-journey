59b9b1f8008390e448b91d935c153e6e2e509eed
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-01-14 16:59:50 +0530

    INLINE style

---
+++ src/INLINE style.jsx
@@ -0,0 +1,90 @@
+function Id(){
+    const card={
+     border:"2px solid #d219193b",
+     width:"200px",
+     boxShadow:"1px 2px 3px 0px #cccccc57",
+     margin:"10px"
+
+    }
+    return(
+        <>
+        <h1>INLINE style in React Js</h1>
+        <div style={{display:"flex",flexWrap:"flex-wrap" }}>
+            <div style={card}>
+                <img style={{width:'200px'}} src="https://png.pngtree.com/png-vector/20240130/ourmid/pngtree-male-cartoon-man-surprising-look-png-image_11506823.png"  />
+
+                <div style={{padding:"10px"}}>
+                   <h3>Devil</h3>
+                   <p>Software developer</p> 
+                </div>
+            </div>
+            <div style={card}>
+                <img style={{width:'200px'}} src="https://png.pngtree.com/png-vector/20240130/ourmid/pngtree-male-cartoon-man-surprising-look-png-image_11506823.png"  />
+
+                <div style={{padding:"10px"}}>
+                   <h3>Devil</h3>
+                   <p>Software developer</p> 
+                </div>
+            </div>
+            <div style={card}>
+                <img style={{width:'200px'}} src="https://png.pngtree.com/png-vector/20240130/ourmid/pngtree-male-cartoon-man-surprising-look-png-image_11506823.png"  />
+
+                <div style={{padding:"10px"}}>
+                   <h3>Devil</h3>
+                   <p>Software developer</p> 
+                </div>
+            </div>
+            <div style={card}>
+                <img style={{width:'200px'}} src="https://png.pngtree.com/png-vector/20240130/ourmid/pngtree-male-cartoon-man-surprising-look-png-image_11506823.png"  />
+
+                <div style={{padding:"10px"}}>
+                   <h3>Devil</h3>
+                   <p>Software developer</p> 
+                </div>
+            </div>
+            <div style={card}>
+                <img style={{width:'200px'}} src="https://png.pngtree.com/png-vector/20240130/ourmid/pngtree-male-cartoon-man-surprising-look-png-image_11506823.png"  />
+
+                <div style={{padding:"10px"}}>
+                   <h3>Devil</h3>
+                   <p>Software developer</p> 
+                </div>
+            </div>
+            <div style={card}>
+                <img style={{width:'200px'}} src="https://png.pngtree.com/png-vector/20240130/ourmid/pngtree-male-cartoon-man-surprising-look-png-image_11506823.png"  />
+
+                <div style={{padding:"10px"}}>
+                   <h3>Devil</h3>
+                   <p>Software developer</p> 
+                </div>
+            </div>
+            <div style={card}>
+                <img style={{width:'200px'}} src="https://png.pngtree.com/png-vector/20240130/ourmid/pngtree-male-cartoon-man-surprising-look-png-image_11506823.png"  />
+
+                <div style={{padding:"10px"}}>
+                   <h3>Devil</h3>
+                   <p>Software developer</p> 
+                </div>
+            </div>
+            <div style={card}>
+                <img style={{width:'200px'}} src="https://png.pngtree.com/png-vector/20240130/ourmid/pngtree-male-cartoon-man-surprising-look-png-image_11506823.png"  />
+
+                <div style={{padding:"10px"}}>
+                   <h3>Devil</h3>
+                   <p>Software developer</p> 
+                </div>
+            </div>
+            <div style={card}>
+                <img style={{width:'200px'}} src="https://png.pngtree.com/png-vector/20240130/ourmid/pngtree-male-cartoon-man-surprising-look-png-image_11506823.png"  />
+
+                <div style={{padding:"10px"}}>
+                   <h3>Devil</h3>
+                   <p>Software developer</p> 
+                </div>
+            </div>
+            
+        </div>
+        </>
+    )
+}
+export default Id;
