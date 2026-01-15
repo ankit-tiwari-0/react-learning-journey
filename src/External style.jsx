ee05826f7171aa670c1ae1f733b2697c8a097d1f
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-01-15 21:01:41 +0530

    External style

---
+++ src/External style.jsx
@@ -0,0 +1,55 @@
+import './css/style.css'
+
+function External(){
+    return(
+        <>
+        <h1 className='heading'>External style</h1>
+        <div className='flex' >
+            <div className='div'>
+                <img className='img-style' src="https://static.vecteezy.com/system/resources/previews/008/191/678/non_2x/cute-girl-cartoon-character-on-white-background-free-vector.jpg"  />
+                <div>
+                  <h4>Noorin </h4>
+                  <p>Teacher</p>  
+                </div>
+            </div>
+            <div className='div'>
+                <img className='img-style' src="https://static.vecteezy.com/system/resources/previews/008/191/678/non_2x/cute-girl-cartoon-character-on-white-background-free-vector.jpg"  />
+                <div>
+                  <h4>Noorin </h4>
+                  <p>Teacher</p>  
+                </div>
+            </div>
+            <div className='div'>
+                <img className='img-style' src="https://static.vecteezy.com/system/resources/previews/008/191/678/non_2x/cute-girl-cartoon-character-on-white-background-free-vector.jpg"  />
+                <div>
+                  <h4>Noorin </h4>
+                  <p>Teacher</p>  
+                </div>
+            </div>
+            <div className='div'>
+                <img className='img-style' src="https://static.vecteezy.com/system/resources/previews/008/191/678/non_2x/cute-girl-cartoon-character-on-white-background-free-vector.jpg"  />
+                <div>
+                  <h4>Noorin </h4>
+                  <p>Teacher</p>  
+                </div>
+            </div>
+            <div className='div'>
+                <img className='img-style' src="https://static.vecteezy.com/system/resources/previews/008/191/678/non_2x/cute-girl-cartoon-character-on-white-background-free-vector.jpg"  />
+                <div>
+                  <h4>Noorin </h4>
+                  <p>Teacher</p>  
+                </div>
+            </div>
+            <div className='div'>
+                <img className='img-style' src="https://static.vecteezy.com/system/resources/previews/008/191/678/non_2x/cute-girl-cartoon-character-on-white-background-free-vector.jpg"  />
+                <div>
+                  <h4>Noorin </h4>
+                  <p>Teacher</p>  
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default External;
