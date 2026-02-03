c52ea2d7e0435fdc4846ee85123259c98ea59d4a
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-02-03 20:18:21 +0530

    complete

--- src/subject.jsx
+++ src/subject.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react"
+import { SubjectContext } from "./ContextApi"
+
 export default function Subject(){
+    const subject=useContext(SubjectContext)
     return(
         <div style={{backgroundColor:"pink",padding:10}}>
-         <h1>Subject  component</h1>
+         <h1>Subject is: {subject}  </h1>
         </div>
     )
 }
