7a7b7937e439a896f4b928895360e99088a87414
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-02-10 22:09:21 +0530

    Install talwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-         <App />
+    <App />
   </BrowserRouter>
-  
-  // <StrictMode>
-  
-  // </StrictMode>,
 )
